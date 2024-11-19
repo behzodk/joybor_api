64ab319b2d7d4fe11232632da7609d17f0be186c
behzodkhan <112095649+behzodkhan@users.noreply.github.com>
2024-11-19 21:58:37 +0500

    Hi

--- properties/urls.py
+++ properties/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import PropertyListCreateView
 
 urlpatterns = [
+    path('', PropertyListCreateView.as_view(), name='property-list-create'),
     path('properties/', PropertyListCreateView.as_view(), name='property-list-create'),
 ]
